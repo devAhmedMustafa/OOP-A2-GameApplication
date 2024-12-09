6e4e17aec250c4b609bf2118a832723be7445a32
Ahmed Mustafa <am1804067@gmail.com>
2024-12-09 12:25:36 +0200

    Add Game Theory utility

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 
+#include "Four-in-row/Board_Four_In_Row.h"
+
 using namespace std;
 int main() {
 
-    cout << "Test 1" << endl;
+    Board_Four_In_Row board;
 
-    cout << "Ana shad" << endl;
+    board.place(1, 'X');
+    board.place(2, 'O');
+    board.place(3, 'X');
+    board.place(4, 'O');
+    board.place(4, 'X');
+    board.place(4, 'O');
+    board.place(3, 'X');
+
+    cout << board.is_win() << endl;
+    board.display_board();
 
     return 0;
 }
