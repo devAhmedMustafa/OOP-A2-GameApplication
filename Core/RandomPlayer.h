8d03052ed4d72ebe3a31d53f1077a6a35be78304
Ahmed Mustafa <61328457+devAhmedMustafa@users.noreply.github.com>
2024-12-11 14:03:49 +0200

    Addd fixed files

--- Core/RandomPlayer.h
+++ Core/RandomPlayer.h
@@ -16,14 +16,3 @@ public:
 	virtual void getmove(int& x, int& y) = 0;
 };
 
-template <typename T>
-class GameManager {
-private:
-	Board<T>* boardPtr;
-	Player<T>* players[2];
-public:
-	GameManager(Board<T>*, Player<T>* playerPtr[2]);
-
-	void run();
-};
-
